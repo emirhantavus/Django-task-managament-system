d9ab4ca65e47b6c30fe3f54b34367ff5ebd7cd5e
Emirhan Tavus <Whisber1@gmail.com>
2024-08-27 21:33:34 +0300

    Add Task model and TDD test cases

--- task_management/tasks/models.py
+++ task_management/tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from projects.models import Project
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
+class Task(models.Model):
+      project = models.ForeignKey(Project,related_name='tasks',on_delete=models.CASCADE)
+      user = models.ForeignKey(User,related_name='tasks',on_delete=models.CASCADE)
+      title = models.CharField(max_length=255)
+      description = models.TextField(blank=True)
+      completed = models.BooleanField(default=False)
+      
+      def __str__(self):
+            return self.title
--- task_management/tasks/tests.py
+++ task_management/tasks/tests.py
@@ -7,8 +7,16 @@ User = get_user_model()
 
 class TaskTestCase(TestCase):
       def setUp(self):
-            self.user = User.objects.create_user(email='testuser@example.com', password='password123')
-            self.project = Project.objects.create(name='Test Project', description='Test project description')
+            self.user = User.objects.create_user(email='user1@mail.com', password='123456')
+            self.project = Project.objects.create(name='project1', description='desc1')
+            self.task = Task.objects.create(
+                        project = self.project,
+                        user = self.user,
+                        title = 'task',
+                        description = 'desc1',
+                        completed = False
+                  )
+            
 
       def test_task_creation(self):
             task = Task.objects.create(
@@ -20,7 +28,43 @@ class TaskTestCase(TestCase):
             self.assertEqual(task.title,'task1')
             self.assertEqual(task.project.name,'project1')
             self.assertEqual(task.user.email,'user1@mail.com')
-            self.assertFalse(self.completed)
       
       def test_task_str_method(self):
-            self.assertEqual(str(self.task),"Task1")
+            self.assertEqual(str(self.task),"task")
+            
+      def test_task_exists(self):
+            self.assertEqual(self.task.title,'task')
+            self.assertEqual(self.task.description,'desc1')
+            self.assertEqual(self.task.user.email,'user1@mail.com')
+            self.assertEqual(self.task.project.name,'project1')
+            
+      def test_update_task(self):
+            self.task.title = 'Updated-title'
+            self.task.save()
+            self.assertEqual(self.task.title,'Updated-title')
+            
+      def test_delete_task(self):
+            task_id = self.task.id
+            #print(Task.objects.get(id=task_id))
+            self.task.delete()
+            with self.assertRaises(Task.DoesNotExist):
+                  print(Task.objects.get(id=task_id))
+      
+      def test_task_completed(self):
+            self.assertFalse(self.task.completed)
+            self.task.completed = True
+            self.task.save()
+            self.assertTrue(self.task.completed)
+            
+      def test_filter_tasks_by_user(self):
+            another_user = User.objects.create_user(email='anotheruser@mail.com',password='123456')
+            Task.objects.create(
+                  project = self.project,
+                  user = another_user,
+                  title = "new_task_title",
+                  description = "new_desc"
+            )
+            tasks = Task.objects.filter(user=another_user)
+            self.assertEqual(tasks[0].user.email,'anotheruser@mail.com')
+            self.assertEqual(tasks.count(),1)
+            self.assertEqual(tasks[0].title,'new_task_title')
